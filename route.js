c209403d9e965d60fea955ab9ba81a11513187ea
FrankXiong <1131076966@qq.com>
2017-04-29 04:49:22 +0800

    update

--- route.js
+++ route.js
@@ -1,11 +1,11 @@
 var path = require('path');
 
 module.exports = function(app) {
-  app.use('/user', require('./api/user'));
-  app.use('/rules', require('./api/rule'));
-  app.use('/counsel', require('./api/counsel'));
-  app.use('/feature', require('./api/feature'));
-	app.use('/*', function (req,res,next) {
+  app.use('/api/user', require('./api/user'));
+  app.use('/api/rules', require('./api/rule'));
+  app.use('/api/counsel', require('./api/counsel'));
+  app.use('/api/feature', require('./api/feature'));
+	app.use('/api/*', function (req,res,next) {
 		return res.json({status:'success',msg:'请求API出错'});
 	})
 };
